7cb2ef92c18ec9a447d49513f7db9e4c809bd510
Dominic-Hulings <dominichulings@gmail.com>
2024-11-13 12:30:46 -0500

    Trying to make the main menu

--- src/window.cpp
+++ src/window.cpp
@@ -1,29 +1,61 @@
+#include <iostream>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
-#include <iostream>
+
+#include "ftxui/component/captured_mouse.hpp"
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/component_base.hpp"
+#include "ftxui/component/screen_interactive.hpp" 
+#include "ftxui/dom/elements.hpp"
 
 #include "window.h"
  
+using std::cout;
+using namespace ftxui;
+
+ButtonOption Style() {
+  auto option = ButtonOption::Animated();
+  option.transform = [](const EntryState& s) {
+    auto element = text(s.label);
+    if (s.focused) {
+      element |= bold;
+    }
+    return element | center | borderEmpty | flex;
+  };
+  return option;
+}
+
 Window::Window()
 {
-  using namespace ftxui;
  
-  // Define the document
-  Element document =
-    vbox ({
+  auto btn1 = Button("start", [&] { cout << "start works\n"; }, Style());
+  auto btn2 = Button("quit", [&] { cout << "quit works\n"; }, Style());
+
+  auto component = Renderer([&] {
+    return vbox ({
       filler(),
-      hbox({
+      filler(),
+      hbox ({
         filler(),
-        text("middle"),
+        text("Blackjack") | center | flex,
         filler()
       }),
       filler(),
+      hbox ({
+        filler(),
+        filler(),
+        btn1->Render(),
+        filler(),
+        btn2->Render(),
+        filler(),
+        filler()
+      }),
+      filler(),
+      filler()
     });
- 
-  auto screen = Screen::Create(
-    Dimension::Full(),       // Width
-    Dimension::Full()        // Height
-  );
-  Render(screen, document);
-  screen.Print();
+  });
+
+  auto screen = ScreenInteractive::Fullscreen();
+
+  screen.Loop(component);
 }
