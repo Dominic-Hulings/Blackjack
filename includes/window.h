310a3c0f5023e934611f7de03cf63400b37eee03
Dominic-Hulings <dominichulings@gmail.com>
2024-11-15 10:31:06 -0500

    fgdksgf

--- includes/window.h
+++ includes/window.h
@@ -4,12 +4,17 @@
 #include <string>
 #include <ftxui/dom/elements.hpp>
 
+#include "game.h"
+
 class Window
 {
   public:
     Window();
   private:
+    void StartGame();
     ftxui::Element DisplaySprite(std::string sprToDisplay);
+
+    ftxui::Screen staticFullScreen;
 };
 
 #endif
--- src/window.cpp
+++ src/window.cpp
@@ -32,7 +32,90 @@ ButtonOption Style() {
 
 Window::Window()
 {
-  auto btn1 = Button("start", [&] { cout << "start works\n"; }, Style());
+  this->staticFullScreen = Screen::Create(Dimension::Full(), Dimension::Full());
+}
+
+Element Window::DisplaySprite(string sprNameToDisplay)
+{
+  string spriteFilePath = string(current_path().parent_path()) + "/sprites/" + sprNameToDisplay + ".txt";
+
+  if (!exists(spriteFilePath))
+  {
+    return text("Sprite not found!");
+  }
+
+  else
+  {
+    ifstream spriteFile(spriteFilePath);
+    string lineRead;
+    vector<Element> documentElements;
+
+    while (getline(spriteFile, lineRead))
+    {
+      documentElements.push_back(hbox(text(lineRead)));
+    }
+
+    spriteFile.close();
+
+    return vbox(documentElements);
+  }
+}
+
+void Window::StartGame()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <stdlib.h>
+#include <vector>
+
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+#include "ftxui/component/captured_mouse.hpp"
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/component_base.hpp"
+#include "ftxui/component/screen_interactive.hpp" 
+#include "ftxui/dom/elements.hpp"
+
+#include "window.h"
+ 
+using std::cout, std::string, std::ifstream, std::pair, std::filesystem::exists, std::filesystem::current_path, std::vector;
+using namespace ftxui;
+
+ButtonOption Style() {
+  auto option = ButtonOption::Animated();
+  option.transform = [](const EntryState& s) {
+    auto element = text(s.label);
+    if (s.focused) {
+      element |= bold;
+    }
+    return element | center | borderEmpty | flex;
+  };
+  return option;
+}
+
+Window::Window()
+{
+  auto btn1 = Button("start", [&] { this->StartGame(); }, Style());
   auto btn2 = Button("quit", [&] { system("clear"); exit(0); }, Style());
 
   auto mainMenu = Container::Horizontal({btn1, btn2}, 0);
@@ -81,6 +164,14 @@ Element Window::DisplaySprite(string sprNameToDisplay)
       documentElements.push_back(hbox(text(lineRead)));
     }
 
+    spriteFile.close();
+
     return vbox(documentElements);
   }
 }
+
+void Window::StartGame()
+{
+
+}
+*/
