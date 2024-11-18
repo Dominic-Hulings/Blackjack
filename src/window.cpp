03113b263bb3a4d076218100de360cd5c49d5e57
Dominic-Hulings <dominichulings@gmail.com>
2024-11-18 12:31:43 -0500

    gotta go

--- src/window.cpp
+++ src/window.cpp
@@ -16,7 +16,7 @@
 
 #include "window.h"
  
-using std::string, std::ifstream, std::pair, std::map, std::vector;
+using std::string, std::ifstream, std::pair, std::map, std::vector, std::filesystem::current_path;
 using namespace ftxui;
 
 Window::Window(string screen)
@@ -44,11 +44,55 @@ void Window::GETscreen(string screen)
 
 Element Window::GETcardSprite(Card cardToGet, int typeOfCard)
 {
+  string spriteFilePath = string(current_path().parent_path()) + "/sprites/cardNums.txt";
   vector<Element> cardSprite;
+  ifstream spriteFile(spriteFilePath);
+  string lineRead;
+  int counter = 4;
+  int otherCounter = 10;
+  bool foundVal = false;
 
   switch(typeOfCard)
   {
     case 1: //* PLAYER CARD 1
+      cardSprite.push_back(hbox(text(" ______________________________")));
+      while(getline(spriteFile, lineRead))
+      {
+        if (!foundVal)
+        {
+          if (lineRead == cardToGet.first)
+          {
+            foundVal = true;
+          }
+
+          else
+          {
+            continue;
+          }
+        }
+
+        else if (counter != 0)
+        {
+          counter--;
+          cardSprite.push_back(hbox(text(lineRead)));
+        }
+
+        else if (otherCounter != 0)
+        {
+          otherCounter--;
+          cardSprite.push_back(hbox(text("|                              |")));
+          counter = 4;
+        }
+
+        else if (counter != 0)
+        {
+          counter--;
+          cardSprite.push_back(hbox(text(lineRead)));
+        }
+      }
+
+      cardSprite.push_back(hbox(text("|______________________________|")));
+      
       break;
     default:
       break;
